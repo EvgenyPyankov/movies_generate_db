c117f9c3f737d6d62f777870514977f8e61efa6c
Евгений <Sleepwalker17@mail.ru>
2016-04-26 21:18:43 +0300

    final

--- src/DBController.java
+++ src/DBController.java
@@ -1,7 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.xml.transform.Result;
+import java.sql.*;
 import java.util.Locale;
 
 public class DBController {
@@ -24,6 +22,90 @@ public class DBController {
             System.out.println("Connection Failed! Check output console");
             return;
         }
+    }
+
+    public void getInfoAboutMovie(int movieId) throws SQLException{
+        PreparedStatement ps;
+        ResultSet rs;
+        String actorsQuery = "select a.name, a.surname from actors a, movies_actors ma where ma.movie_id = ? and a.id=ma.actor_id";
+        String movieQuery="select name from movies where id = ?";
+        String ratingsQuery="select r.name, mr.value\n" +
+                "from ratings r, movies_ratings mr\n" +
+                "where mr.movie_id = ?\n" +
+                "and r.id=mr.rating_id";
+        String genresQuery="select g.name\n" +
+                "from genres g, movies_genres mg\n" +
+                "where mg.movie_id = ?\n" +
+                "and g.id= mg.genre_id";
+
+        ps = conn.prepareStatement(movieQuery);
+        ps.setLong(1,movieId);
+        rs = ps.executeQuery();
+       // System.out.println("ID: "+movieId);
+        //if (rs.next() )System.out.println(rs.getString("name"));
+
+        ps = conn.prepareStatement(actorsQuery);
+        ps.setInt(1,movieId);
+        rs =  ps.executeQuery();
+        //System.out.println("Actors:");
+        while(rs.next()){
+            String name = rs.getString("name");
+            String surname = rs.getString("surname");
+
+            //System.out.println(name+" "+surname);
+        }
+
+        ps = conn.prepareStatement(genresQuery);
+        ps.setInt(1,movieId);
+        rs =  ps.executeQuery();
+       // System.out.println("Genres:");
+        while(rs.next()){
+            String name = rs.getString("name");
+
+          //  System.out.println(name);
+        }
+
+        ps = conn.prepareStatement(ratingsQuery);
+        ps.setInt(1,movieId);
+        rs =  ps.executeQuery();
+       // System.out.println("Ratings:");
+        while(rs.next()){
+            String name = rs.getString("name");
+            int value = rs.getInt("value");
+
+            //Display values
+           // System.out.println(name+": "+value);
+        }
+
+    }
+
+    public void run() throws SQLException{
+        String query;
+        long startTime;
+        long endTime;
+        startTime = System.currentTimeMillis();
+        getInfoAboutMovie(1000);
+        endTime = System.currentTimeMillis();
+        System.out.println(("Get all info about movie: "+(endTime-startTime)+" millis"));
+
+        query = "Select name from movies where id =1000";
+        startTime = System.nanoTime();
+        stmt.executeQuery(query);
+        endTime = System.nanoTime();
+        System.out.println(("Search for a singe field by movie id: "+(endTime-startTime)+" nanos"));
+
+        query = "Select * from movies where id =1000";
+        startTime = System.nanoTime();
+        stmt.executeQuery(query);
+        endTime = System.nanoTime();
+        System.out.println(("Search for all fields by movie id: "+(endTime-startTime)+" nanos"));
+
+        query = "Select * from movies where name like 'Movie #1000'";
+        startTime = System.currentTimeMillis();
+        stmt.executeQuery(query);
+        endTime = System.currentTimeMillis();
+        System.out.println(("Search for all fields by certain field value: "+(endTime-startTime)+" millis"));
+
     }
 
     public Connection getConnection(){
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         final int N = 10000;
 
-        Generator generator = new Generator();
-        generator.generate();
+//        Generator generator = new Generator();
+//        generator.generate();
 
-   }
+        DBController dbController = new DBController();
+        try {
+            //dbController.getInfoAboutMovie(1000);
+            dbController.run();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
